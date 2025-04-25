8dddd1fbf2ee23efb451814f233a3374f0c329ec
ItsAltoo <Itsmalik57@gmail.com>
2025-04-25 20:02:27 +0800

    25.4.25

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -97,7 +97,12 @@ WSGI_APPLICATION = "mywebsite.wsgi.app"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
