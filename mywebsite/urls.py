230e557e92b3e375c1c9eef27ec7d476440307c5
ItsAltoo <itsmalik57@gmail.com>
2025-07-13 08:21:29 +0800

    Comment out the reload URL for django_browser_reload in urls.py

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
     path("", include("home.urls"), name="home"),
     path("accounts/", include("accounts.urls")),
 ]
